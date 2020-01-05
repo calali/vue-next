03e199688052f4eebeda443a09d2d98cb1d7c3ef
liqiuhui <cala_li@163.com>
2020-01-05 20:30:10 +0800

    feat:add comment

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  preset: 'ts-jest',
-  globals: {
+  preset: 'ts-jest',//使用ts-jest的预设
+  globals: {//在所有测试环境中都可以使用的全局变量
     __DEV__: true,
     __TEST__: true,
     __VERSION__: require('./package.json').version,
@@ -10,8 +10,8 @@ module.exports = {
     __FEATURE_OPTIONS__: true,
     __FEATURE_SUSPENSE__: true
   },
-  coverageDirectory: 'coverage',
-  coverageReporters: ['html', 'lcov', 'text'],
+  coverageDirectory: 'coverage',//输出覆盖率的文件
+  coverageReporters: ['html', 'lcov', 'text'],//生成覆盖率报告的时候Jest使用的报告名字
   collectCoverageFrom: [
     'packages/*/src/**/*.ts',
     '!packages/runtime-test/src/utils/**',
